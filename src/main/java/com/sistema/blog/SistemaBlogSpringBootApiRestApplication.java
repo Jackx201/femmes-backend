b99171fc8e3ff6395816e5b8c5fb8412b08305e5
Jackx201 <zahuldmz@live.com>
2023-05-26 12:40:17 -0600

    Kafka Imports removed and DB Password added as env var

--- src/main/java/com/sistema/blog/SistemaBlogSpringBootApiRestApplication.java
+++ src/main/java/com/sistema/blog/SistemaBlogSpringBootApiRestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class SistemaBlogSpringBootApiRestApplication {
